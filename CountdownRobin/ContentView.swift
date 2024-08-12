409fab1021d7a25fbeb1f88983df4a3be0714fb3
Seungeun Lee <slee32268@gmail.com>
2024-08-11 20:34:26 -0700

    Initial Commit

---
+++ CountdownRobin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountdownRobin
+//
+//  Created by Seung Eun Lee on 8/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountdownRobin/CountdownRobinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownRobinApp.swift
+//  CountdownRobin
+//
+//  Created by Seung Eun Lee on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownRobinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
